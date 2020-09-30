51524f4df7ccadf62f3adac93b440a678838c68c
Gustavo <elmergustavo79@gmail.com>
2020-09-30 17:28:35 -0600

    Metodo de Gaus Jordan

---
+++ src/gaussjordan/Inicio.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gaussjordan;
+
+/**
+ *
+ * @author Enterprise Systro
+ */
+public class Inicio {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Prinipal me=new Prinipal();
+        me.setLocationRelativeTo(me);
+        me.setVisible(true);
+    }
+    
+}
